55497ecadb46bbc9e8c10d9bc0ee8529b6f4028a
leilarouabhi <rouabhi-leila@hotmail.fr>
2023-12-05 11:54:17 +0100

    "version 2 get_post"

--- afficher_liste.js
+++ afficher_liste.js
@@ -88,7 +88,7 @@
 //console.log(dataJson);        
             post("http://fbrc.esy.es/DWWM22053/Api/api.php/users", dataJson);
         
-            
+            location.reload()
             
             
         } catch (err) {
